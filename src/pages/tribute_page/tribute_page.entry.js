7611a98abb179031faab987ce2259d381729d6eb
Paul Serban <paul.alex.serban@gmail.com>
2021-07-27 16:29:55 +0300

    fcc-rwd_17 | updated | tribute-page html

--- src/pages/tribute_page/tribute_page.entry.js
+++ src/pages/tribute_page/tribute_page.entry.js
@@ -2,8 +2,8 @@ require('normalize.css/normalize.css');
 import './tribute_page.styles.scss';
 
 // import STRUCTURE
-import headerStructure from '../../ui_ux/structure/header/header';
-import navigationStructure from '../../ui_ux/structure/navigation/navigation';
+import headerStructure from '../../ui_ux/layout/header/header';
+import navigationStructure from '../../ui_ux/layout/navigation/navigation';
 
 // init STRUCTURE
 new headerStructure;
