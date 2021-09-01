0653685bc59621d572f7293707980d22d723cbc2
Paul Serban <paul.alex.serban@gmail.com>
2021-09-01 16:59:56 +0300

    fcc-rwd_31 | updated | html to comply to fcc-rwd tests

--- src/pages/personal_portfolio/personal_portfolio.entry.js
+++ src/pages/personal_portfolio/personal_portfolio.entry.js
@@ -1,3 +1,7 @@
 import './personal_portfolio.styles.scss'
 
+// JS and STRUCTURE / LAYOUT
+import headerStructure from '../../ui_ux/layout/header/header'; 
+new headerStructure;
+
 console.log('hello personal portfolio');
